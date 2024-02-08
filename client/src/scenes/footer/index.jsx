eea0b9acd88af7a7c5265622717a3d1a9cf78e84
Shanmukh025 <shanmukh2564@gmail.com>
2024-02-08 10:51:07 +0530

    Version 1.0

--- client/src/scenes/footer/index.jsx
+++ client/src/scenes/footer/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const index = () => {
     return (
         <>
-            <footer>
+            <footer style={{ textAlign: "center", marginBottom: "10px" }}>
                 <h3>Spark by Shanmukh.</h3>
             </footer>
         </>
