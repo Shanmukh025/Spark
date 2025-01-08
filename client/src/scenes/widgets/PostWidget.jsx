125b489d99ada83348976259f45d992d1df91910
Shanmukh025 <shanmukh2564@gmail.com>
2025-01-08 17:50:24 +0530

    Commit

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -82,7 +82,6 @@ const PostWidget = ({
         setNewComment("");
     };
 
-    const formattedDate = "25 Jun 2024";
 
     const handleShareTwitter = (post) => {
         const url = `https://www.twitter.com/compose/post?text=Checkout this New Post from Spark :
