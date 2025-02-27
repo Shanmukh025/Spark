0099a664b1cd6ff6858bd94906147c60215c25ed
Shanmukh Anaparthi <110324769+Shanmukh025@users.noreply.github.com>
2025-02-27 11:48:36 +0530

    Update UserImage.jsx

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
                 width={size}
                 height={size}
                 alt="user"
-                src={`https://spark-production-744e.up.railway.app/assets/${image}`}
+                src={`https://xspark-production.up.railway.app/assets/${image}`}
             />
         </Box>
     );
