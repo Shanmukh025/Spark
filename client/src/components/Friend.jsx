2e7badc2f1fac9070da2d1777fce5f40c9cf1389
Shanmukh Anaparthi <110324769+Shanmukh025@users.noreply.github.com>
2025-02-27 11:48:17 +0530

    Update Friend.jsx

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
     const patchFriend = async () => {
         const response = await fetch(
-            `https://spark-production-744e.up.railway.app/users/${_id}/${friendId}`,
+            `https://xspark-production.up.railway.app/users/${_id}/${friendId}`,
             {
                 method: "PATCH",
                 headers: {
