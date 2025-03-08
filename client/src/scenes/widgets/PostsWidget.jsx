3f6bd26d223b2fddaf6e15b74b72a203fb0dd77f
Shanmukh Anaparthi <110324769+Shanmukh025@users.noreply.github.com>
2025-03-08 13:29:32 +0530

    Update PostsWidget.jsx

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -22,7 +22,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
 
     const getUserPosts = async () => {
         const response = await fetch(
-            `https://xspark-production.up.railway.app/posts/${userId}/posts`,
+            `https://spark-yag0.onrender.com/posts/${userId}/posts`,
             {
                 method: "GET",
                 headers: { Authorization: `Bearer ${token}` },
